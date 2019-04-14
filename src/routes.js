dfc730d09305e6d892bda0e95ac0e13e44280777
Moreno Laforet <laforetmoreno@gmail.com>
2019-04-14 18:42:35 -0300

    Up

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const routes = require("express").Router();
-const { create, retrieveAll } = require("./controllers/users");
+// const userController = require("./controllers/users");
 
 const User = require("./models/User");
 const { transporter, mailOptions } = require("./controllers/mailer");
@@ -10,53 +10,53 @@ routes.get("/", (req, res) => {
 });
 
 // Users;
-routes.post("/users", create());
-routes.get("/users", retrieveAll());
+// routes.post("/users", userController.create);
+// routes.get("/users", userController.retrieveAll);
 // routes.get("/users/:id", userController.retrieveById);
 // routes.delete("/users/:id", userController.deleteUser);
 
-// routes.post("/users", async (req, res) => {
-//   const { name, email, message, subject } = req.body;
+routes.post("/users", async (req, res) => {
+  const { name, email, message, subject } = req.body;
 
-//   if (subject !== "lead") {
-//     transporter.sendMail(
-//       mailOptions(MAILER_EMAIL, email, name, message, subject),
-//       function(error, info) {
-//         if (error) {
-//           console.log(error);
-//         } else {
-//           console.info("Send Email: " + info.response);
-//         }
-//       }
-//     );
-//   }
+  if (subject !== "lead") {
+    transporter.sendMail(
+      mailOptions(MAILER_EMAIL, email, name, message, subject),
+      function(error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.info("Send Email: " + info.response);
+        }
+      }
+    );
+  }
 
-//   const allUsers = await User.find();
+  const allUsers = await User.find();
 
-//   const existingUsers = allUsers
-//     .map(existingUser => existingUser.email)
-//     .filter(userMail => userMail === email);
+  const existingUsers = allUsers
+    .map(existingUser => existingUser.email)
+    .filter(userMail => userMail === email);
 
-//   if (existingUsers.length > 0) {
-//     res.status(409).send("User already exists.");
-//   } else {
-//     await User.create({
-//       email
-//     });
+  if (existingUsers.length > 0) {
+    res.status(409).send("User already exists.");
+  } else {
+    await User.create({
+      email
+    });
 
-//     res.status(200).send("Created");
-//   }
-// });
+    res.status(200).send("Created");
+  }
+});
 
-// routes.get("/users", async (req, res) => {
-//   const users = await User.find();
+routes.get("/users", async (req, res) => {
+  const users = await User.find();
 
-//   try {
-//     return res.json(users);
-//   } catch (error) {
-//     return res.status(404).send(error, "No users found.");
-//   }
-// });
+  try {
+    return res.json(users);
+  } catch (error) {
+    return res.status(404).send(error, "No users found.");
+  }
+});
 
 routes.get("/users/:id", async (req, res) => {
   const user = await User.findById(req.params.id);

06a347c1862d8e120b877b12cd474fb9cb7f4bc2
Moreno Laforet <laforetmoreno@gmail.com>
2019-04-14 18:15:36 -0300

    Test

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const routes = require("express").Router();
-const userController = require("./controllers/users");
+// const userController = require("./controllers/users");
+const User = require("../models/User");
 
 // Heathcheck
 routes.get("/", (req, res) => {
@@ -7,9 +8,18 @@ routes.get("/", (req, res) => {
 });
 
 // Users;
-routes.post("/users", userController.create);
-routes.get("/users", userController.retrieveAll);
-routes.get("/users/:id", userController.retrieveById);
-routes.delete("/users/:id", userController.deleteUser);
+// routes.post("/users", userController.create);
+routes.get("/users", async (req, res) => {
+  const users = await User.find();
+
+  try {
+    return res.json(users);
+  } catch (error) {
+    return res.status(404).send(error, "No users found.");
+  }
+});
+
+// routes.get("/users/:id", userController.retrieveById);
+// routes.delete("/users/:id", userController.deleteUser);
 
 module.exports = routes;

9054c490d9c33eacc50e5f341496c5a028c1b012
Moreno Laforet <laforetmoreno@gmail.com>
2019-04-14 18:27:25 -0300

    Add controllers

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const routes = require("express").Router();
-// const userController = require("./controllers/users");
-const User = require("./models/User");
+const userController = require("./controllers/users");
 
 // Heathcheck
 routes.get("/", (req, res) => {
@@ -8,18 +7,9 @@ routes.get("/", (req, res) => {
 });
 
 // Users;
-// routes.post("/users", userController.create);
-routes.get("/users", async (req, res) => {
-  const users = await User.find();
-
-  try {
-    return res.json(users);
-  } catch (error) {
-    return res.status(404).send(error, "No users found.");
-  }
-});
-
-// routes.get("/users/:id", userController.retrieveById);
-// routes.delete("/users/:id", userController.deleteUser);
+routes.post("/users", userController.create);
+routes.get("/users", userController.retrieveAll);
+routes.get("/users/:id", userController.retrieveById);
+routes.delete("/users/:id", userController.deleteUser);
 
 module.exports = routes;
